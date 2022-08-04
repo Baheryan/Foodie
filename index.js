8291898b329ed79525d6a47476c12fa4c680cf91
Baheryan Serhiy <baheryan.s@gmail.com>
2022-08-04 16:47:02 +0300

    add script for footer form validation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//mobile menu animation
+
 const buttonMenu = document.getElementById('header__button');
 
 buttonMenu.onclick = function() {
@@ -5,4 +7,43 @@ buttonMenu.onclick = function() {
     menuElements.forEach((menuElements) => {
         menuElements.classList.toggle('active');
     });
-}
+};
+
+//footer form validation
+
+const form = document.getElementById("footer__form");
+const input = document.getElementById("form__input");
+const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const formMessage = document.getElementById("form__message");
+
+form.addEventListener("submit", element => {
+    element.preventDefault();
+    validation();
+    submitStatus();
+});
+
+const validation = function () {
+   if (input.value.match(pattern)) {
+    formMessage.classList.add("valid");
+    formMessage.classList.remove("invalid");
+    formMessage.innerHTML = "Your Email Adress Is Valid";
+   } else {
+    formMessage.classList.remove("valid");
+    formMessage.classList.add("invalid");
+    formMessage.innerHTML = "Please Enter Valid Email Adress";
+   }
+};
+
+const submitStatus = function() {
+    if (input.value.match(pattern)) {
+        formMessage.innerHTML = "Congrats! You've been subsribed";
+    }
+
+    if (input.value === '') {
+        formMessage.innerHTML = "Email filed cannot be empty";
+    }
+};
+
+input.addEventListener("focusout", element => {
+    formMessage.innerHTML = "";
+});
